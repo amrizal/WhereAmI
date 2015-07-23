3159e28c051b349d8fdf3388c5430b5b1defb3b7
Amrizal <amrizal@outlook.com>
2015-07-23 17:25:38 +0800

    Page 536

--- app/src/main/java/com/example/amrizalzainuddin/whereami/WhereAmI.java
+++ app/src/main/java/com/example/amrizalzainuddin/whereami/WhereAmI.java
@@ -1,7 +1,9 @@
 package com.example.amrizalzainuddin.whereami;
 
 import android.content.Context;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,6 +12,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class WhereAmI extends ActionBarActivity {
@@ -66,13 +72,38 @@ public class WhereAmI extends ActionBarActivity {
         myLocationText = (TextView)findViewById(R.id.myLocationText);
 
         String latLongString = "No location found";
+        String addressString = "No address found";
+
         if(location != null){
             double lat = location.getLatitude();
             double lng = location.getLongitude();
             latLongString = "Lat:" + lat + "\nLong:" + lng;
+
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            Geocoder gc = new Geocoder(this, Locale.getDefault());
+
+            try{
+                List<Address> addresses = gc.getFromLocation(latitude, longitude, 1);
+                StringBuilder sb = new StringBuilder();
+                if(addresses.size() > 0){
+                    Address address = addresses.get(0);
+
+                    for(int i=0; i<address.getMaxAddressLineIndex(); i++)
+                        sb.append(address.getAddressLine(i)).append("\n");
+
+                    sb.append(address.getLocality()).append("\n");
+                    sb.append(address.getPostalCode()).append("\n");
+                    sb.append(address.getCountryName());
+
+                    addressString = sb.toString();
+                }
+            }catch (IOException e){}
         }
 
-        myLocationText.setText("Your Current Position is:\n" + latLongString);
+        myLocationText.setText("Your Current Position is:\n" + latLongString
+                                + "\n\n" + addressString);
     }
 
     @Override
